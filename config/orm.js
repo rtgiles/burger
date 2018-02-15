aecfbba1c6872e41997baf00df6e9d12771ff291
rtgiles <rtgiles@msn.com>
2018-02-14 20:17:28 -0600

    first commit

---
+++ config/orm.js
@@ -0,0 +1,64 @@
+// Import Node Dependencies
+var connection = require('./connection.js');
+
+
+
+
+// Connect to MySQL database
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  };
+  console.log('connected as id ' + connection.threadId);
+});
+
+
+
+
+// Methods for MySQL commands
+var orm = {
+
+  // selectAll()
+  selectAll: function(callback) {
+
+    // Run MySQL Query
+    connection.query('SELECT * FROM burgers', function (err, result) {
+      if (err) throw err;
+      callback(result);
+    });
+
+  },
+
+  // insertOne()
+  insertOne: function(burger_name, callback){
+
+  // Run MySQL Query
+     connection.query('INSERT INTO burgers SET ?', {
+       burger_name: burger_name,
+       devoured: false,
+       created_at: timestamp
+     }, function (err, result) {
+       if (err) throw err;
+       callback(result);
+     });
+
+   },
+
+  // updateOne()
+  updateOne: function(burgerID, callback){
+
+    // Run MySQL Query
+    connection.query('UPDATE burgers SET ? WHERE ?', [{devoured: true}, {id: burgerID}], function (err, result) {
+        if (err) throw err;
+        callback(result);
+      });
+
+  }
+
+};
+
+
+
+// Export the ORM object in module.exports.
+module.exports = orm;
