22ac62ad3e3704b20f6500b1e38665d8c5f72c15
rtgiles <rtgiles@msn.com>
2018-03-08 23:29:08 -0600

    final app

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ var path= require('path');
 var exphbs= require ('express-handlebars');
 
 var app= express();
-var PORT= 3000;
+var PORT= process.env.PORT || 3000;
 
 // app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: true}));
 // app.use(bodyParser.text());
 // app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
